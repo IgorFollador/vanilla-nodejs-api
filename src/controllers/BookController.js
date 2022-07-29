e252fd9abdc7acf27217d471a9d2f400e28ed028
Igor Follador <igorledf@hotmail.com>
2022-07-28 23:19:27 -0300

    Read all data file

---
+++ src/controllers/BookController.js
@@ -0,0 +1,27 @@
+const db = require('../database/data');
+
+class BookController {
+    
+    async readBooks() {
+        return new Promise((resolve, _) => resolve(db));
+    }
+
+    // async createBook(formBook) {
+    //     return new Promise((resolve, _) => {
+    //         let newId = data.find((todo) => { 
+                
+    //             todo.id === parseInt(id)}
+    //         );
+
+    //         let newBook = {
+    //             id: newId, 
+    //             ...formBook
+    //         };
+
+    //         resolve(newBook);
+    //     })
+    // } 
+
+}
+
+module.exports = BookController;
