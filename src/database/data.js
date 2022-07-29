1cc14682a96f3413b493f1caefabed9438064fa5
Igor Follador <igorledf@hotmail.com>
2022-07-28 23:05:55 -0300

    Create and populate data file

---
+++ src/database/data.js
@@ -0,0 +1,24 @@
+const books = [
+    {
+        id: 1,
+        title: "Journey to the Center of The Earth",
+        author: "Jules Verne"
+    },
+    {
+        id: 2,
+        title: "Twenty Thousand Leagues Under the Sea",
+        author: "Jules Verne"
+    },
+    {
+        id: 3,
+        title: "The Posthmous Memoirs of Bras Cubas",
+        author: "Machado de Assis"
+    },
+    {
+        id: 4,
+        title: "A Brief History of Time",
+        author: "Stephen Hawking"
+    }
+];
+
+module.exports = books;
